3b9f1c49ec2e673f353696c6a90fb97f7ca2c3c5
Avi Aryan <avi.aryan123@gmail.com>
2017-02-09 11:57:55 +0530

    feat: show episode number with links, add defaults

--- kissanime.js
+++ kissanime.js
@@ -12,9 +12,9 @@ var login = "vergo777";
 var api_key = "R_6a13f014b38f4f80a31cf7d80a7c18c7";
 var long_url; 
 
-var startEpisode; 
+var startEpisode;
 do {
-	startEpisode = prompt("Enter episode number you want to start from");
+	startEpisode = prompt("Enter episode number you want to start from", defaultText="1");
 	if(startEpisode <= 0 || startEpisode > episodeLinks.length) {
 		alert("Episode number entered must be greater than 0 and lesser than total number of eps"); 
 	} else {
@@ -24,58 +24,41 @@ do {
 
 var endEpisode; 
 do {
-	endEpisode = prompt("Enter episode number you want to end at");
+	endEpisode = prompt("Enter episode number you want to end at", defaultText="2");
 	if(endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
 		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
 	} else {
 		break;
 	}
-} while(true); 
-var videoQuality = prompt("Enter video quality you want to download. Example - '960x720.mp4' (without the quotes)"); 
+} while(true);
 
-var i; 
+var videoQuality = prompt(
+	"Enter video quality you want to download. Example - '960x720.mp4' (without the quotes)", 
+	defaultText="854x480.mp4"
+);
+
+var i;
 for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEpisode); i--) {
 	jQuery.ajax({
          url:    URL + episodeLinks[i], 
          success: function(result) {
                     var $result = eval($(result));
-					// var stringStart = result.search("var wra"); 
-					// var stringEnd = result.search("document.write"); 
-					// var javascriptToExecute = result.substring(stringStart, stringEnd);
-					// eval(javascriptToExecute);
 					
-					console.log(result.search("Save link as"));
-					console.log(result.search("divDownload"));
+					// console.log(result.search("Save link as"));
+					// console.log(result.search("divDownload"));
 
 					var data = $(result).find("#divDownload");  // download data
 					var links = $(data[0]).find("a");
 					// console.log(links);
 					
-					// $("body").append('<div id="episode' + i + '" style="display: none;"></div>')
-					// $('#episode' + i).append(wra);
-					
-					// var downloadQualityOptions = $('#episode' + i + ' a').map(function(i,el) { return $(el); });
-					$.each(links, function(i, el) {
-						console.log(el);
+					$.each(links, function(index, el) {
+						// console.log(el);
 						if (videoQuality == $(el).html()){
 							long_url = $(el).attr('href');
+							console.log('Episode ' + (episodeLinks.length - i));
 							console.log(long_url);
 						}
 					});
-
-					var downloadQualityOptions = $(data).map(function(i,el) { return $(el); });
-					var j;
-					for(j = 0; j < downloadQualityOptions.length; j++) {
-						// debug
-						console.log(downloadQualityOptions[j].length());
-
-						if(videoQuality === downloadQualityOptions[j].html()) {
-							long_url = downloadQualityOptions[j].attr('href');
-							console.log(i);
-							console.log(long_url);
-							// get_short_url(long_url, login, api_key);
-						}
-					}
                   },
          async:   false, 
 		 script:  true
@@ -83,8 +66,7 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 }
 
 
-function get_short_url(long_url, login, api_key)
-{
+function get_short_url(long_url, login, api_key){
     $.getJSON(
         "http://api.bitly.com/v3/shorten?callback=?", 
         { 
